8cd8e02e89deb7e2a61f4f62d0d21b5b6128dd95
youyou li <yoyoleeisstrong@gmail.com>
2016-10-15 23:49:37 +0800

    Assignment of R 2.0

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+# This function could cache the reversible matrix, and return a list of functions
+makeCacheMatrix <- function(x = numeric()) {
+# initial 'i'
+# 'i' would cache the reversible matrix
+        i <- NULL
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-        inverse <- NULL
-        set <- function(y) {
-                x <<- y
-                inverse <<- NULL
-        }
+# get the 'x' value
         get <- function() x
-        setInverse <- function(mean) inverse <<- solve(x)
-        getInverse <- function() inverse
+# set the inversible matrix
+        setInverse <- function(inverse) i <<- inverse
+# get the cached inversible matrix
+        getInverse <- function() i
+# return a list of functions
         list(set = set, get = get,
              setInverse = setInverse,
              getInverse = getInverse)
 }
-
-
-
-## Write a short comment describing this function
-
+# This function could calculate reversible matrix while the reversible matrix is
+# not exist
 cacheSolve <- function(x, ...) {
+# get the inversible value from x which is the list of functions
         m <- x$getInverse()
+# if inversible value was exist, then return it
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
+# if inversible value was not exist, then calculate it
+# first, get the matrix defined by 'x'
         data <- x$get()
-        m <- solve(data, ...)
-        x$setInverse(m)
+# second, calculate the inversible matrix defined by'x'
+        i <- solve(data, ...)
+# third, cache the inversible value defined by 'x'
+        x$setInverse(i)
         m
 }
